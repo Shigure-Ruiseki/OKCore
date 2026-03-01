9c92bbf106b1e3212d8fe8cab6e4e29cfd6517a2
Ruisu <akitomoya2908@gmail.com>
2026-03-01 19:01:31 +0700

    Initial commit

---
+++ src/main/java/ruiseki/okcore/Reference.java
@@ -0,0 +1,12 @@
+package ruiseki.okcore;
+
+public class Reference {
+
+    public static final String MOD_ID = Tags.MOD_ID;
+    public static final String MOD_NAME = Tags.MOD_NAME;
+    public static final String VERSION = Tags.VERSION;
+    public static final String DEPENDENCIES = "";
+    public static final String PROXY_COMMON = Tags.MOD_GROUP + ".CommonProxy";
+    public static final String PROXY_CLIENT = Tags.MOD_GROUP + ".ClientProxy";
+    public static final String GUI_FACTORY = Tags.MOD_GROUP + ".config.OKGuiConfigFactory";
+}

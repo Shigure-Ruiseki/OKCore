e7e781ea43b16ae51393f4b77c0db16e30723c44
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-04-05 20:58:13 +0800

    rename item to items

--- src/main/java/ruiseki/okcore/client/OKCGuiTextures.java
+++ src/main/java/ruiseki/okcore/client/OKCGuiTextures.java
@@ -7,32 +7,32 @@ import ruiseki.okcore.Reference;
 public class OKCGuiTextures {
 
     public static final UITexture EMPTY_BATTERY_INPUT = UITexture.builder()
-        .location(Reference.MOD_ID, "item/empty_battery_input_slot")
+        .location(Reference.MOD_ID, "items/empty_battery_input_slot")
         .imageSize(16, 16)
         .build();
 
     public static final UITexture EMPTY_BATTERY_OUTPUT = UITexture.builder()
-        .location(Reference.MOD_ID, "item/empty_battery_output_slot")
+        .location(Reference.MOD_ID, "items/empty_battery_output_slot")
         .imageSize(16, 16)
         .build();
 
     public static final UITexture EMPTY_TANK_INPUT = UITexture.builder()
-        .location(Reference.MOD_ID, "item/empty_tank_input_slot")
+        .location(Reference.MOD_ID, "items/empty_tank_input_slot")
         .imageSize(16, 16)
         .build();
 
     public static final UITexture EMPTY_TANK_OUTPUT = UITexture.builder()
-        .location(Reference.MOD_ID, "item/empty_tank_output_slot")
+        .location(Reference.MOD_ID, "items/empty_tank_output_slot")
         .imageSize(16, 16)
         .build();
 
     public static final UITexture EMPTY_UPGRADE = UITexture.builder()
-        .location(Reference.MOD_ID, "item/empty_upgrade_slot")
+        .location(Reference.MOD_ID, "items/empty_upgrade_slot")
         .imageSize(16, 16)
         .build();
 
     public static final UITexture INACCESSIBLE_SLOT = UITexture.builder()
-        .location(Reference.MOD_ID, "item/inaccessible_slot")
+        .location(Reference.MOD_ID, "items/inaccessible_slot")
         .imageSize(16, 16)
         .build();
 }

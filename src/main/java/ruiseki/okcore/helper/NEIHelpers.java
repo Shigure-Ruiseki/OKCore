f2bd9909a6454a3ab51274c90ec76ec7c32948a5
Shigure-Ruiseki <pn163163@gmail.com>
2026-05-09 21:46:21 +0700

    add nei addon helper

---
+++ src/main/java/ruiseki/okcore/helper/NEIHelpers.java
@@ -0,0 +1,67 @@
+package ruiseki.okcore.helper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidContainerRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.IFluidBlock;
+import net.minecraftforge.fluids.IFluidContainerItem;
+
+public class NEIHelpers {
+
+    public static String translate(String unlocalized) {
+        return StatCollector.translateToLocal("neiintegration." + unlocalized);
+    }
+
+    public static List<ItemStack> getItemVariations(ItemStack base) {
+        List<ItemStack> variations = new ArrayList<>();
+        base.getItem()
+            .getSubItems(base.getItem(), null, variations);
+        Iterator<ItemStack> itr = variations.iterator();
+        ItemStack stack;
+        while (itr.hasNext()) {
+            stack = itr.next();
+            if (!base.isItemEqual(stack) || !stack.hasTagCompound()) {
+                itr.remove();
+            }
+        }
+        if (variations.isEmpty()) {
+            return Collections.singletonList(base);
+        }
+        return variations;
+    }
+
+    public static FluidStack getFluidStack(ItemStack stack) {
+        if (stack != null) {
+            FluidStack fluidStack = null;
+            if (stack.getItem() instanceof IFluidContainerItem) {
+                fluidStack = ((IFluidContainerItem) stack.getItem()).getFluid(stack);
+            }
+            if (fluidStack == null) {
+                fluidStack = FluidContainerRegistry.getFluidForFilledItem(stack);
+            }
+            if (fluidStack == null && Block.getBlockFromItem(stack.getItem()) instanceof IFluidBlock) {
+                Fluid fluid = ((IFluidBlock) Block.getBlockFromItem(stack.getItem())).getFluid();
+                if (fluid != null) {
+                    fluidStack = new FluidStack(fluid, 1000);
+                }
+            }
+            return fluidStack;
+        }
+        return null;
+    }
+
+    public static boolean areFluidsSameType(FluidStack fluidStack1, FluidStack fluidStack2) {
+        if (fluidStack1 == null || fluidStack2 == null) {
+            return false;
+        }
+        return fluidStack1.getFluid() == fluidStack2.getFluid();
+    }
+}

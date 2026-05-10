58dacddf7c176ae3f7de1490690b6be988bf3a0b
Shigure-Ruiseki <pn163163@gmail.com>
2026-05-10 16:13:10 +0700

    fix depen

--- src/main/java/ruiseki/okcore/Reference.java
+++ src/main/java/ruiseki/okcore/Reference.java
@@ -6,7 +6,20 @@ public class Reference {
     public static final String MOD_NAME = Tags.MOD_NAME;
     public static final String VERSION = Tags.VERSION;
     public static final String DEPENDENCIES = "required-after:gtnhlib@[0.9.59,);"
-        + "required-after:modularui2@[2.3.56-1.7.10,);";
+        + "required-after:modularui2@[2.3.56-1.7.10,);"
+        + "after:Baubles|Expanded;"
+        + "after:Baubles;"
+        + "after:NotEnoughItems;"
+        + "after:Waila;"
+        + "after:Botania;"
+        + "after:TConstruct;"
+        + "after:EnderIO;"
+        + "after:ThermalFoundation;"
+        + "after:MinefactoryReloaded;"
+        + "after:Mekanism;"
+        + "after:BigReactors;"
+        + "after:ActuallyAdditions;"
+        + "after:DraconicEvolution;";
     public static final String PROXY_COMMON = Tags.MOD_GROUP + ".CommonProxy";
     public static final String PROXY_CLIENT = Tags.MOD_GROUP + ".ClientProxy";
     public static final String GUI_FACTORY = Tags.MOD_GROUP + ".config.GuiConfigFactory";

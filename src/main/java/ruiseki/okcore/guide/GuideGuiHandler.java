11bdcc618587a826c73609a97daeca525e175e9e
Shigure-Ruiseki <pn163163@gmail.com>
2026-06-22 14:26:04 +0700

    fix 2?

--- src/main/java/ruiseki/okcore/guide/GuideGuiHandler.java
+++ src/main/java/ruiseki/okcore/guide/GuideGuiHandler.java
@@ -8,6 +8,7 @@ import cpw.mods.fml.common.network.IGuiHandler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import ruiseki.okcore.guide.capability.CapabilityGuide;
+import ruiseki.okcore.guide.capability.IGuideHandler;
 import ruiseki.okcore.guide.gui.GuiCategory;
 import ruiseki.okcore.guide.gui.GuiEntry;
 import ruiseki.okcore.guide.gui.GuiHome;
@@ -33,31 +34,34 @@ public class GuideGuiHandler implements IGuiHandler {
     @SideOnly(Side.CLIENT)
     public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
         if (!MinecraftHelpers.isClientSide()) return null;
-        return EntityHelpers.getCapability(player, CapabilityGuide.GUIDE_CAPABILITY)
-            .map(cap -> {
-                Book book = GuideHelpers.getIndexedBooks()
-                    .get(ID);
-                try {
-                    String lastEntry = cap.getLastEntry();
-                    int lastCategoryIdx = cap.getLastCategory();
-
-                    if (lastCategoryIdx >= 0 && lastCategoryIdx < book.getCategories()
-                        .size()) {
-                        CategoryAbstract category = book.getCategories()
-                            .get(lastCategoryIdx);
-
-                        if (lastEntry != null && !lastEntry.isEmpty()
-                            && category.entries.containsKey(new ResourceLocation(lastEntry))) {
-                            EntryAbstract entry = category.entries.get(new ResourceLocation(lastEntry));
-                            return new GuiEntry(book, category, entry, player);
-                        }
-
-                        return new GuiCategory(book, category, player, null);
-                    }
-                } catch (Exception ignore) {}
-
-                return new GuiHome(book, player);
-            })
+        IGuideHandler cap = EntityHelpers.getCapability(player, CapabilityGuide.GUIDE_CAPABILITY)
+            .map(s -> s)
             .orElse(null);
+        if (cap == null) return null;
+
+        Book book = GuideHelpers.getIndexedBooks()
+            .get(ID);
+        if (book == null) return null;
+
+        try {
+            String lastEntry = cap.getLastEntry();
+            int lastCategoryIdx = cap.getLastCategory();
+
+            if (lastCategoryIdx >= 0 && lastCategoryIdx < book.getCategories()
+                .size()) {
+                CategoryAbstract category = book.getCategories()
+                    .get(lastCategoryIdx);
+
+                if (lastEntry != null && !lastEntry.isEmpty()
+                    && category.entries.containsKey(new ResourceLocation(lastEntry))) {
+                    EntryAbstract entry = category.entries.get(new ResourceLocation(lastEntry));
+                    return new GuiEntry(book, category, entry, player);
+                }
+
+                return new GuiCategory(book, category, player, null);
+            }
+        } catch (Exception ignore) {}
+
+        return new GuiHome(book, player);
     }
 }

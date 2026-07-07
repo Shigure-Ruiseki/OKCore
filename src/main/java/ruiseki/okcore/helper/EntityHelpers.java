f460d7030260fbfec057d797509f755b6bdb822d
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-07-07 20:09:09 +0700

    ✨ feat(entity): Add methods to set entity facing
    Introduces helper methods to calculate yaw and pitch from ForgeDirection, and a function to set an entity's rotation based on a given direction.

--- src/main/java/ruiseki/okcore/helper/EntityHelpers.java
+++ src/main/java/ruiseki/okcore/helper/EntityHelpers.java
@@ -1,6 +1,8 @@
 package ruiseki.okcore.helper;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.util.ForgeDirection;
 
 import ruiseki.okcore.entity.cooldown.ICooldownHandler;
 import ruiseki.okcore.entity.cooldown.ItemCooldowns;
@@ -17,5 +19,44 @@ public class EntityHelpers {
         } catch (ClassCastException ignored) {
             return null;
         }
+    }
+
+    public static float getYawFromFacing(ForgeDirection facing) {
+        switch (facing) {
+            case NORTH:
+                return 180F;
+            case WEST:
+                return 90F;
+            case EAST:
+                return 270F;
+            case SOUTH:
+            default:
+                return 0F;
+        }
+    }
+
+    public static float getPitchFromFacing(ForgeDirection facing) {
+        switch (facing) {
+            case DOWN:
+                return 90F;
+            case UP:
+                return -90F;
+            default:
+                return 0F;
+        }
+    }
+
+    public static void setEntityFacing(EntityLivingBase entity, ForgeDirection currentFacing) {
+        float yaw = getYawFromFacing(currentFacing);
+        float pitch = getPitchFromFacing(currentFacing);
+
+        entity.rotationYaw = yaw;
+        entity.rotationPitch = pitch;
+
+        entity.prevRotationYaw = yaw;
+        entity.prevRotationPitch = pitch;
+
+        entity.rotationYawHead = yaw;
+        entity.prevRotationYawHead = yaw;
     }
 }

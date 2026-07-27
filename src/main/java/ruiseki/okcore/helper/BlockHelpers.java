51e68738bf940582db0a41bd3b34e51fbfeba2ca
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-07-27 22:11:04 +0700

    ✨ feat(block): Add block state serialization helpers
    Adds methods to serialize and deserialize block states to/from string and meta pairs. This enables saving and loading block state information.

--- src/main/java/ruiseki/okcore/helper/BlockHelpers.java
+++ src/main/java/ruiseki/okcore/helper/BlockHelpers.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.Nullable;
+
+import com.gtnewhorizon.gtnhlib.blockstate.core.BlockState;
+import com.gtnewhorizon.gtnhlib.blockstate.registry.BlockPropertyRegistry;
 
 import ruiseki.okcore.datastructure.BlockPos;
 
@@ -72,5 +78,59 @@ public class BlockHelpers {
     public static boolean isValidCreativeTab(Block block, @Nullable CreativeTabs creativeTab) {
         return creativeTab == null || creativeTab == CreativeTabs.tabAllSearch
             || block.getCreativeTabToDisplayOn() == creativeTab;
+    }
+
+    /**
+     * Convert the given blockstate to a pair of blockname string and meta value.
+     * 
+     * @param blockState The blockstate to serialize.
+     * @return The pair of the blockname and meta value.
+     */
+    public static Pair<String, Integer> serializeBlockState(BlockState blockState) {
+        String blockName = Block.blockRegistry.getNameForObject(blockState.getBlock())
+            .toString();
+        int meta = blockState.getPropertyValue("meta");
+        return Pair.of(blockName, meta);
+    }
+
+    /**
+     * Convert the given serialized blockstate to a blockstate instance.
+     * 
+     * @param serializedBlockState The pair of the blockname and meta value.
+     * @return The resulting blockstate. Can be null if the referred block does not exist.
+     */
+    public static BlockState deserializeBlockState(Pair<String, Integer> serializedBlockState) {
+        Block block = Block.getBlockFromName(serializedBlockState.getLeft());
+        int meta = serializedBlockState.getRight() != null ? serializedBlockState.getRight() : 0;
+        if (block != null) {
+            ItemStack stack = new ItemStack(block, 1, meta);
+            return BlockPropertyRegistry.getBlockState(stack);
+        }
+        return null;
+    }
+
+    /**
+     * Get the blockstate from the given stack
+     * 
+     * @param stack The stack
+     * @return The blockstate
+     */
+    public static BlockState getBlockStateFromItemStack(ItemStack stack) {
+        return BlockPropertyRegistry.getBlockState(stack);
+    }
+
+    /**
+     * Get the itemstack from the given blockstate
+     * 
+     * @param blockState The blockstate
+     * @return The itemstack
+     */
+    public static ItemStack getItemStackFromBlockState(BlockState blockState) {
+        Item item = Item.getItemFromBlock(blockState.getBlock());
+        if (item == null) {
+            return null;
+        }
+        int meta = blockState.getPropertyValue("meta");
+        return new ItemStack(item, 1, meta);
     }
 }

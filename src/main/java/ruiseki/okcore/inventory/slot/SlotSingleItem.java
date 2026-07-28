8adb2f571c765d95cbf727a254eeddd0b323266c
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-07-28 15:56:04 +0700

    ✨ feat(slot): Add SlotSingleItem for single item acceptance
    Introduces `SlotSingleItem`, a new slot type that only accepts a specific item, improving inventory management and validation.

---
+++ src/main/java/ruiseki/okcore/inventory/slot/SlotSingleItem.java
@@ -0,0 +1,39 @@
+package ruiseki.okcore.inventory.slot;
+
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import lombok.Getter;
+
+/**
+ * Slot that is used for only accepting one item.
+ * 
+ * @author rubensworks
+ *
+ */
+public class SlotSingleItem extends SlotExtended {
+
+    @Getter
+    private Item item;
+
+    /**
+     * Make a new instance.
+     * 
+     * @param inventory The inventory this slot will be in.
+     * @param index     The index of this slot.
+     * @param x         X coordinate.
+     * @param y         Y coordinate.
+     * @param item      The item to accept.
+     */
+    public SlotSingleItem(IInventory inventory, int index, int x, int y, Item item) {
+        super(inventory, index, x, y);
+        this.item = item;
+    }
+
+    @Override
+    public boolean isItemValid(ItemStack itemStack) {
+        return super.isItemValid(itemStack) && itemStack.getItem() == getItem();
+    }
+
+}

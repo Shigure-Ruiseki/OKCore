7dd918865ba8652dad1aead8cc9ed62f004dd513
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-07-28 15:38:52 +0700

    ✨ feat(model): Add DirectionCorner enum for block positioning
    Introduces a new enum `DirectionCorner` to represent directional offsets in 3D space,
    extending the functionality of `EnumFacing` for block position calculations.
    ✨ feat(config): Add ConfigurableBlockGui for blocks with GUIs
    
    Introduces `ConfigurableBlockGui`, an abstract block class that extends
    `ConfigurableBlock` and implements `IGuiContainerProvider` for blocks
    with associated GUIs and `ExtendedConfig` data.

---
+++ src/main/java/ruiseki/okcore/config/configurable/ConfigurableBlockGui.java
@@ -0,0 +1,64 @@
+package ruiseki.okcore.config.configurable;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
+import ruiseki.okcore.config.extendedconfig.ExtendedConfig;
+import ruiseki.okcore.init.ModBase;
+import ruiseki.okcore.inventory.IGuiContainerProvider;
+import ruiseki.okcore.network.PacketHandler;
+
+/**
+ * Block without a tile entity with a GUI that can hold ExtendedConfigs.
+ * The container and GUI must be set inside the constructor of the extension.
+ * 
+ * @author rubensworks
+ *
+ */
+public abstract class ConfigurableBlockGui extends ConfigurableBlock implements IGuiContainerProvider {
+
+    private int guiID;
+
+    /**
+     * Make a new block instance.
+     * 
+     * @param eConfig  Config for this blockState.
+     * @param material Material of this blockState.
+     */
+    @SuppressWarnings({ "rawtypes" })
+    public ConfigurableBlockGui(ExtendedConfig eConfig, Material material) {
+        super(eConfig, material);
+        this.hasGui = true;
+        if (hasGui()) {
+            this.guiID = PacketHandler.getNewId(eConfig.getMod(), PacketHandler.IDType.GUI);
+        }
+    }
+
+    @Override
+    public int getGuiID() {
+        return this.guiID;
+    }
+
+    @Override
+    public ModBase getMod() {
+        return getConfig().getMod();
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float subX,
+        float subY, float subZ) {
+        super.onBlockActivated(world, x, y, z, player, side, subX, subY, subZ);
+
+        // Drop through if the player is sneaking
+        if (player.isSneaking()) {
+            return false;
+        }
+
+        if (!world.isRemote && hasGui()) {
+            player.openGui(getConfig().getMod(), getGuiID(), world, x, y, z);
+        }
+
+        return true;
+    }
+}

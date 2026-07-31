5a35ed61c2b3ed9e163a149e401d28d620318392
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-07-31 17:11:01 +0700

    add Inventory Test

--- src/main/java/ruiseki/okcore/Configs.java
+++ src/main/java/ruiseki/okcore/Configs.java
@@ -3,11 +3,13 @@ package ruiseki.okcore;
 import ruiseki.okcore.config.ConfigHandler;
 import ruiseki.okcore.core.ItemEnergyTestConfig;
 import ruiseki.okcore.core.ItemFluidTestConfig;
+import ruiseki.okcore.core.ItemInventoryTestConfig;
 
 public class Configs {
 
     public static void register(ConfigHandler configHandler) {
         configHandler.add(new ItemEnergyTestConfig());
         configHandler.add(new ItemFluidTestConfig());
+        configHandler.add(new ItemInventoryTestConfig());
     }
 }

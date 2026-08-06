3e380c8e622545ee5e7535824a103fcfcf10a422
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-08-06 20:13:49 +0700

    ✨ refactor(block/collidable): Simplify ray tracing and collision logic
    Refactors `CollidableComponent` to streamline ray tracing and collision detection. Updates method signatures to use world coordinates (x, y, z) instead of `BlockPos` for better clarity and consistency.

--- src/main/java/ruiseki/okcore/block/collidable/ICollidable.java
+++ src/main/java/ruiseki/okcore/block/collidable/ICollidable.java
@@ -10,11 +10,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import lombok.Data;
-import ruiseki.okcore.datastructure.BlockPos;
 
 /**
  * Interface for blocks that have a collidable component.
@@ -33,54 +33,45 @@ public interface ICollidable<P> {
     /**
      * Add the current block bounding box to the given list.
      *
-     * @param world            The world
-     * @param pos              The position
-     * @param mask             The bounding boxes mask
-     * @param list             The list to add to
-     * @param collidingEntity  The entity that is colliding
-     * @param useProvidedState If the given state should be used instead of the actual world state.
+     * @param world           The world
+     * @param x,              y, z The position
+     * @param mask            The bounding boxes mask
+     * @param list            The list to add to
+     * @param collidingEntity The entity that is colliding
      */
-    public void addCollisionBoxToList(World world, BlockPos pos, AxisAlignedBB mask, List<AxisAlignedBB> list,
-        Entity collidingEntity, boolean useProvidedState);
+    public void addCollisionBoxesToList(World world, int x, int y, int z, AxisAlignedBB mask, List<AxisAlignedBB> list,
+        Entity collidingEntity);
 
     /**
-     * The selected bounding box.
+     * The the selected bounding box.
      *
-     * @param worldIn The world
-     * @param pos     The position
+     * @param world The world
+     * @param x,    y, z The position
      * @return The selected bounding box
      */
-    public AxisAlignedBB getSelectedBoundingBox(World worldIn, BlockPos pos);
+    @SideOnly(Side.CLIENT)
+    public AxisAlignedBB getSelectedBoundingBoxFromPool(World world, int x, int y, int z);
 
     /**
      * Do a ray trace for the current look direction of the player.
      *
      * @param world  The world.
-     * @param pos    The block position to perform a ray trace for.
+     * @param x,     y, z The block position to perform a ray trace for.
      * @param player The player.
      * @return A holder object with information on the ray tracing.
      */
-    public RayTraceResult<P> doRayTrace(World world, BlockPos pos, EntityPlayer player);
+    public RayTraceResult<P> doRayTrace(World world, int x, int y, int z, EntityPlayer player);
 
     /**
      * Ray trace the given direction.
      *
      * @param world     The world
-     * @param pos       The position
+     * @param x,        y, z The position
      * @param origin    The origin vector
      * @param direction The direction vector
      * @return The position object holder
      */
-    public MovingObjectPosition collisionRayTrace(World world, BlockPos pos, Vec3 origin, Vec3 direction);
-
-    /**
-     * Get the bounding box.
-     *
-     * @param world The world
-     * @param pos   The position
-     * @return The bounding box
-     */
-    public AxisAlignedBB getBoundingBox(IBlockAccess world, BlockPos pos);
+    public MovingObjectPosition collisionRayTrace(World world, int x, int y, int z, Vec3 origin, Vec3 direction);
 
     /**
      * Result from ray tracing
@@ -113,23 +104,22 @@ public interface ICollidable<P> {
 
         public int getBoundsCount(P position);
 
-        public boolean isActive(B block, World world, BlockPos pos, P position);
+        public boolean isActive(B block, World world, int x, int y, int z, P position);
 
-        public List<AxisAlignedBB> getBounds(B block, World world, BlockPos pos, P position);
+        public List<AxisAlignedBB> getBounds(B block, World world, int x, int y, int z, P position);
 
-        public ItemStack getPickBlock(World world, BlockPos pos, P position);
+        public ItemStack getPickBlock(World world, int x, int y, int z, P position);
 
         /**
          * Destroy this component
          *
-         * @param world     The world
-         * @param pos       The position
-         * @param position  The component position
-         * @param player    The player destroying the component.
-         * @param saveState If the component state should be saved in the dropped item.
+         * @param world    The world
+         * @param x,       y, z The position
+         * @param position The component position
+         * @param player   The player destroying the component.
          * @return If the complete block was destroyed
          */
-        public boolean destroy(World world, BlockPos pos, P position, EntityPlayer player, boolean saveState);
+        public boolean destroy(World world, int x, int y, int z, P position, EntityPlayer player);
     }
 
 }
--- src/main/java/ruiseki/okcore/block/collidable/ICollidableParent.java
+++ src/main/java/ruiseki/okcore/block/collidable/ICollidableParent.java
@@ -1,5 +1,8 @@
 package ruiseki.okcore.block.collidable;
 
+import java.util.List;
+
+import net.minecraft.entity.Entity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
@@ -7,34 +10,45 @@ import net.minecraft.world.World;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import ruiseki.okcore.datastructure.BlockPos;
 
 /**
  * Interface used to access the parent methods from a {@link ICollidable}.
- * 
+ *
  * @author rubensworks
  */
 public interface ICollidableParent {
 
     /**
      * Simply forward this call to the super.
-     * 
-     * @param worldIn The world
-     * @param pos     The position
-     * @return The selected bounding box
+     *
+     * @param worldIn         The world
+     * @param x,              y, z The position
+     * @param mask            The bounding boxes mask
+     * @param list            The list to add to
+     * @param collidingEntity The entity that is colliding
      */
-    @SideOnly(Side.CLIENT)
-    public AxisAlignedBB getSelectedBoundingBoxParent(World worldIn, BlockPos pos);
+    public void addCollisionBoxesToListParent(World worldIn, int x, int y, int z, AxisAlignedBB mask,
+        List<AxisAlignedBB> list, Entity collidingEntity);
 
     /**
      * Simply forward this call to the super.
-     * 
-     * @param pos         The position
-     * @param start       The start vector
-     * @param end         The end vector
-     * @param boundingBox The bounding box to ray trace with.
+     *
+     * @param worldIn The world
+     * @param x,      y, z The position
+     * @return The selected bounding box
+     */
+    @SideOnly(Side.CLIENT)
+    public AxisAlignedBB getSelectedBoundingBoxFromPoolParent(World worldIn, int x, int y, int z);
+
+    /**
+     * Simply forward this call to the super.
+     *
+     * @param world     The world
+     * @param x,        y, z The position
+     * @param origin    The origin vector
+     * @param direction The direction vector
      * @return The position object holder
      */
-    public MovingObjectPosition rayTraceParent(BlockPos pos, Vec3 start, Vec3 end, AxisAlignedBB boundingBox);
+    public MovingObjectPosition collisionRayTraceParent(World world, int x, int y, int z, Vec3 origin, Vec3 direction);
 
 }

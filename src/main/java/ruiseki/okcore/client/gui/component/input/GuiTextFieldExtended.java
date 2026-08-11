c889b140896a47519604f6850599b6c4151141fc
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-08-11 19:01:40 +0700

    ✨ feat(GuiTextFieldExtended): Add constructor and mouse click handling
    Introduces a new constructor for convenience and adds right-click functionality to select all text.

--- src/main/java/ruiseki/okcore/client/gui/component/input/GuiTextFieldExtended.java
+++ src/main/java/ruiseki/okcore/client/gui/component/input/GuiTextFieldExtended.java
@@ -9,7 +9,7 @@ import ruiseki.okcore.client.renderer.GlStateManager;
 
 /**
  * An extended text field.
- * 
+ *
  * @author rubensworks
  */
 public class GuiTextFieldExtended extends GuiTextField {
@@ -23,6 +23,10 @@ public class GuiTextFieldExtended extends GuiTextField {
         this.background = background;
     }
 
+    public GuiTextFieldExtended(int componentId, FontRenderer fontrenderer, int x, int y, int width, int height) {
+        this(componentId, fontrenderer, x, y, width, height, false);
+    }
+
     public void setListener(IInputListener listener) {
         this.listener = listener;
     }
@@ -31,6 +35,8 @@ public class GuiTextFieldExtended extends GuiTextField {
         minecraft.renderEngine.bindTexture(Images.WIDGETS);
         GlStateManager.color(1, 1, 1, 1);
 
+        xPosition--;
+        yPosition--;
         drawTexturedModalRect(xPosition, yPosition, 0, 0, width / 2, height / 2);// top left
         drawTexturedModalRect(xPosition + width / 2, yPosition, 200 - width / 2, 0, width / 2, height / 2);// top right
         drawTexturedModalRect(xPosition, yPosition + height / 2, 0, 20 - height / 2, width / 2, height / 2);// bottom
@@ -42,6 +48,8 @@ public class GuiTextFieldExtended extends GuiTextField {
             20 - height / 2,
             width / 2,
             height / 2);// bottom right
+        xPosition++;
+        yPosition++;
     }
 
     @Override
@@ -52,7 +60,7 @@ public class GuiTextFieldExtended extends GuiTextField {
 
     /**
      * Draw the text box with mouse sensitive parameters.
-     * 
+     *
      * @param minecraft Minecraft instance.
      * @param mouseX    Mouse X
      * @param mouseY    Mouse Y
@@ -69,4 +77,17 @@ public class GuiTextFieldExtended extends GuiTextField {
         super.drawTextBox();
     }
 
+    @Override
+    public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
+        if (mouseButton == 1 && mouseX >= this.xPosition
+            && mouseX < this.xPosition + this.width
+            && mouseY >= this.yPosition
+            && mouseY < this.yPosition + this.height) {
+            // Select everything
+            this.setFocused(true);
+            this.setCursorPosition(0);
+            this.setSelectionPos(Integer.MAX_VALUE);
+        }
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
 }

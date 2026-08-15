f55cf701aec67337e4307d420ae16a348fa0249c
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-08-15 22:18:20 +0700

    rollback

--- src/main/java/ruiseki/okcore/config/configurable/ConfigurableBlockLog.java
+++ src/main/java/ruiseki/okcore/config/configurable/ConfigurableBlockLog.java
@@ -2,7 +2,8 @@ package ruiseki.okcore.config.configurable;
 
 import java.util.List;
 
-import net.minecraft.block.material.Material;
+import net.minecraft.block.BlockLog;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
@@ -13,45 +14,45 @@ import net.minecraftforge.common.util.ForgeDirection;
 import com.gtnewhorizon.gtnhlib.blockstate.core.BlockState;
 import com.gtnewhorizon.gtnhlib.geometry.Axis;
 
+import lombok.experimental.Delegate;
 import ruiseki.okcore.block.property.BlockProperty;
+import ruiseki.okcore.block.property.BlockPropertyProviderComponent;
+import ruiseki.okcore.block.property.IBlockPropertyProvider;
 import ruiseki.okcore.block.property.IEnumProperty;
 import ruiseki.okcore.config.extendedconfig.BlockConfig;
 import ruiseki.okcore.config.extendedconfig.ExtendedConfig;
 import ruiseki.okcore.datastructure.BlockPos;
 import ruiseki.okcore.helper.BlockHelpers;
 
-public class ConfigurableBlockLog extends ConfigurableBlock {
+public class ConfigurableBlockLog extends BlockLog implements IConfigurableBlock, IBlockPropertyProvider {
 
-    @BlockProperty
-    public static final IEnumProperty<Axis> AXIS = IEnumProperty
-        .construct("axis", Axis.class, Axis.Y, (world, x, y, z) -> {
-            int meta = world.getBlockMetadata(x, y, z);
-            return switch (meta & 0b1100) {
-                case 0b0100 -> Axis.X;
-                case 0b1000 -> Axis.Z;
-                default -> Axis.Y;
-            };
-        }, (world, x, y, z, value) -> {
-            int currentMeta = world.getBlockMetadata(x, y, z);
-            int axisBits = switch (value) {
-                case X -> 0b0100;
-                case Z -> 0b1000;
-                default -> 0b0000; // Axis.Y
-            };
-            int newMeta = (currentMeta & ~0b1100) | axisBits;
-            world.setBlockMetadataWithNotify(x, y, z, newMeta, 2);
-        });
+    @Delegate
+    protected IBlockPropertyProvider propertyProvider = new BlockPropertyProviderComponent(this);
+
+    // For ItemBlock
+    @BlockProperty(allowBlock = false)
+    private static final IEnumProperty<Axis> AXIS = IEnumProperty.construct("axis", Axis.class, Axis.Y);
 
     protected BlockConfig eConfig = null;
     protected boolean hasGui = false;
 
     public ConfigurableBlockLog(ExtendedConfig<BlockConfig> eConfig) {
-        super(eConfig, Material.wood);
+        this.setConfig((BlockConfig) eConfig);
+        this.setBlockName(eConfig.getUnlocalizedName());
     }
 
     @Override
     public boolean hasGui() {
         return hasGui;
+    }
+
+    private void setConfig(BlockConfig eConfig) {
+        this.eConfig = eConfig;
+    }
+
+    @Override
+    public BlockConfig getConfig() {
+        return eConfig;
     }
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
@@ -62,10 +63,29 @@ public class ConfigurableBlockLog extends ConfigurableBlock {
     }
 
     @Override
+    public void registerBlockIcons(IIconRegister reg) {
+
+    }
+
+    @Override
+    public int onBlockPlaced(World worldIn, int x, int y, int z, int side, float subX, float subY, float subZ,
+        int meta) {
+        return meta;
+    }
+
+    @Override
     public BlockState getStateForPlacement(World world, BlockPos pos, ForgeDirection facing, float hitX, float hitY,
         float hitZ, int meta, EntityLivingBase placer) {
-        BlockState state = super.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, placer);
-        state.setPropertyValue(AXIS, Axis.fromDirection(facing));
+        BlockState state = IConfigurableBlock.super.getStateForPlacement(
+            world,
+            pos,
+            facing,
+            hitX,
+            hitY,
+            hitZ,
+            meta,
+            placer);
+        state.setPropertyValue("axis", Axis.fromDirection(facing));
         return state;
     }
 }

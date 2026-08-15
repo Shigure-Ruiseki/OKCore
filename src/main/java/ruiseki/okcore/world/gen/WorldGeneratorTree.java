bb84a4dfa125ae76e9869df4f8a1b2b30d051644
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-08-15 22:19:43 +0700

    roll back WorldGeneratorTree

--- src/main/java/ruiseki/okcore/world/gen/WorldGeneratorTree.java
+++ src/main/java/ruiseki/okcore/world/gen/WorldGeneratorTree.java
@@ -3,6 +3,8 @@ package ruiseki.okcore.world.gen;
 import java.util.Random;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockLeaves;
+import net.minecraft.block.BlockLog;
 import net.minecraft.block.BlockSapling;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
@@ -52,9 +54,9 @@ public abstract class WorldGeneratorTree extends WorldGenerator {
 
     protected abstract int baseHeightRandomRange();
 
-    public abstract Block getLeaves();
+    public abstract BlockLeaves getLeaves();
 
-    public abstract Block getLogs();
+    public abstract BlockLog getLogs();
 
     public abstract BlockSapling getSapling();
 

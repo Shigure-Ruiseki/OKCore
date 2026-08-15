acb6b38183a778396da12f9d9bff4f370bfd6df3
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-08-15 22:24:22 +0700

    rollback ConfigurableBlockLog again

--- src/main/java/ruiseki/okcore/config/configurable/ConfigurableBlockLog.java
+++ src/main/java/ruiseki/okcore/config/configurable/ConfigurableBlockLog.java
@@ -2,43 +2,55 @@ package ruiseki.okcore.config.configurable;
 
 import java.util.List;
 
-import net.minecraft.block.BlockLog;
-import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
 import com.gtnewhorizon.gtnhlib.blockstate.core.BlockState;
 import com.gtnewhorizon.gtnhlib.geometry.Axis;
 
-import lombok.experimental.Delegate;
 import ruiseki.okcore.block.property.BlockProperty;
-import ruiseki.okcore.block.property.BlockPropertyProviderComponent;
-import ruiseki.okcore.block.property.IBlockPropertyProvider;
 import ruiseki.okcore.block.property.IEnumProperty;
 import ruiseki.okcore.config.extendedconfig.BlockConfig;
 import ruiseki.okcore.config.extendedconfig.ExtendedConfig;
 import ruiseki.okcore.datastructure.BlockPos;
 import ruiseki.okcore.helper.BlockHelpers;
 
-public class ConfigurableBlockLog extends BlockLog implements IConfigurableBlock, IBlockPropertyProvider {
+public class ConfigurableBlockLog extends ConfigurableBlock {
 
-    @Delegate
-    protected IBlockPropertyProvider propertyProvider = new BlockPropertyProviderComponent(this);
-
-    // For ItemBlock
-    @BlockProperty(allowBlock = false)
-    private static final IEnumProperty<Axis> AXIS = IEnumProperty.construct("axis", Axis.class, Axis.Y);
+    @BlockProperty
+    public static final IEnumProperty<Axis> AXIS = IEnumProperty
+        .construct("axis", Axis.class, Axis.Y, (world, x, y, z) -> {
+            int meta = world.getBlockMetadata(x, y, z);
+            return switch (meta & 0b1100) {
+                case 0b0100 -> Axis.X;
+                case 0b1000 -> Axis.Z;
+                default -> Axis.Y;
+            };
+        }, (world, x, y, z, value) -> {
+            int currentMeta = world.getBlockMetadata(x, y, z);
+            int axisBits = switch (value) {
+                case X -> 0b0100;
+                case Z -> 0b1000;
+                default -> 0b0000;
+            };
+            int newMeta = (currentMeta & ~0b1100) | axisBits;
+            world.setBlockMetadataWithNotify(x, y, z, newMeta, 2);
+        });
 
     protected BlockConfig eConfig = null;
     protected boolean hasGui = false;
 
     public ConfigurableBlockLog(ExtendedConfig<BlockConfig> eConfig) {
-        this.setConfig((BlockConfig) eConfig);
-        this.setBlockName(eConfig.getUnlocalizedName());
+        super(eConfig, Material.wood);
+        this.setHardness(2.0F);
+        this.setStepSound(soundTypeWood);
     }
 
     @Override
@@ -63,29 +75,38 @@ public class ConfigurableBlockLog extends BlockLog implements IConfigurableBlock
     }
 
     @Override
-    public void registerBlockIcons(IIconRegister reg) {
-
-    }
-
-    @Override
-    public int onBlockPlaced(World worldIn, int x, int y, int z, int side, float subX, float subY, float subZ,
-        int meta) {
-        return meta;
-    }
-
-    @Override
     public BlockState getStateForPlacement(World world, BlockPos pos, ForgeDirection facing, float hitX, float hitY,
         float hitZ, int meta, EntityLivingBase placer) {
-        BlockState state = IConfigurableBlock.super.getStateForPlacement(
-            world,
-            pos,
-            facing,
-            hitX,
-            hitY,
-            hitZ,
-            meta,
-            placer);
-        state.setPropertyValue("axis", Axis.fromDirection(facing));
+        BlockState state = super.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, placer);
+        state.setPropertyValue(AXIS, Axis.fromDirection(facing));
         return state;
+    }
+
+    public void breakBlock(World worldIn, int x, int y, int z, Block blockBroken, int meta) {
+        byte b0 = 4;
+        int i1 = b0 + 1;
+
+        if (worldIn.checkChunksExist(x - i1, y - i1, z - i1, x + i1, y + i1, z + i1)) {
+            for (int j1 = -b0; j1 <= b0; ++j1) {
+                for (int k1 = -b0; k1 <= b0; ++k1) {
+                    for (int l1 = -b0; l1 <= b0; ++l1) {
+                        Block block = worldIn.getBlock(x + j1, y + k1, z + l1);
+                        if (block.isLeaves(worldIn, x + j1, y + k1, z + l1)) {
+                            block.beginLeavesDecay(worldIn, x + j1, y + k1, z + l1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean canSustainLeaves(IBlockAccess world, int x, int y, int z) {
+        return true;
+    }
+
+    @Override
+    public boolean isWood(IBlockAccess world, int x, int y, int z) {
+        return true;
     }
 }

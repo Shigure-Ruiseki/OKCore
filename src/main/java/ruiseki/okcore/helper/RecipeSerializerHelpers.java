44685dc5e07fcd36b516f548795f45869a0f40bf
GitHub GTNH Actions <>
2026-08-24 14:52:02 +0000

    spotlessApply

--- src/main/java/ruiseki/okcore/helper/RecipeSerializerHelpers.java
+++ src/main/java/ruiseki/okcore/helper/RecipeSerializerHelpers.java
@@ -12,7 +12,6 @@ import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTException;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidContainerRegistry;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.oredict.OreDictionary;

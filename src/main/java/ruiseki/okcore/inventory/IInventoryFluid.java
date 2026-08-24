623e9ac2471e51e69f1ce248aacbe7c8b459ee86
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-08-24 16:29:16 +0700

    ✨ feat(inventory): Add IInventoryFluid interface
    Introduces a new interface, IInventoryFluid, which extends IInventory and
    provides a method to get an IFluidHandler. This allows inventories to also
    contain fluids.

---
+++ src/main/java/ruiseki/okcore/inventory/IInventoryFluid.java
@@ -0,0 +1,14 @@
+package ruiseki.okcore.inventory;
+
+import net.minecraft.inventory.IInventory;
+import ruiseki.okcore.fluid.handler.IFluidHandler;
+
+/**
+ * An inventory that can also contain fluids.
+ * @author rubensworks
+ */
+public interface IInventoryFluid extends IInventory {
+
+    public IFluidHandler getFluidHandler();
+
+}

d0ade0044df521fbe236a96c9a76bfd22b4b3425
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-09-03 20:35:02 +0700

    ✨ refactor(config): Prevent duplicate ExtendedConfig instantiation
    This change adds a check to prevent duplicate instantiation of ExtendedConfig. It also ensures that if a previous instance exists, its

--- src/main/java/ruiseki/okcore/config/extendedconfig/ExtendedConfig.java
+++ src/main/java/ruiseki/okcore/config/extendedconfig/ExtendedConfig.java
@@ -109,17 +109,20 @@ public abstract class ExtendedConfig<C extends ExtendedConfig<C, I>, I>
     protected String getConfigPropertyPrefix() {
         return this.getNamedId();
     }
-
     /**
      * Save this config inside the correct element and inside the implementation of itself.
      */
+    @SuppressWarnings("unchecked")
     public void save() {
         try {
-            Field field = this.getClass()
-                .getField("_instance");
+            if (this.instance != null) {
+                return;
+            }
+
+            Field field = this.getClass().getField("_instance");
             Object currentInstance = field.get(null);
 
-            if (currentInstance != null && currentInstance != this) {
+            if (currentInstance != null) {
                 ExtendedConfig<?, ?> prevConfig = (ExtendedConfig<?, ?>) currentInstance;
                 if (prevConfig.getInstance() != null) {
                     this.instance = (I) prevConfig.getInstance();

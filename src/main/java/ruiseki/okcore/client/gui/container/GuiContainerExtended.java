537f91397d1bc65294892cf6262f7bb18134c41a
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-09-17 12:33:51 +0700

    fix drawScreen

--- src/main/java/ruiseki/okcore/client/gui/container/GuiContainerExtended.java
+++ src/main/java/ruiseki/okcore/client/gui/container/GuiContainerExtended.java
@@ -7,16 +7,22 @@ import java.util.List;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
 
 import org.jetbrains.annotations.Nullable;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import com.google.common.collect.Lists;
 
@@ -93,12 +99,109 @@ public abstract class GuiContainerExtended<T extends ExtendedInventoryContainer>
         return 166;
     }
 
+    /**
+     * Draws the screen and all the components in it.
+     */
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+        int k = this.guiLeft;
+        int l = this.guiTop;
+        this.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+        RenderHelper.disableStandardItemLighting();
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+
         super.drawScreen(mouseX, mouseY, partialTicks);
         for (IRenderable renderable : this.renderables) {
             renderable.drawScreen(mouseX, mouseY, partialTicks);
         }
+
+        RenderHelper.enableGUIStandardItemLighting();
+        GL11.glPushMatrix();
+        GL11.glTranslatef((float) k, (float) l, 0.0F);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+        this.theSlot = null;
+        short short1 = 240;
+        short short2 = 240;
+        OpenGlHelper
+            .setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, short1, short2);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        int k1;
+
+        for (int i1 = 0; i1 < this.inventorySlots.inventorySlots.size(); ++i1) {
+            Slot slot = this.inventorySlots.inventorySlots.get(i1);
+            this.func_146977_a(slot);
+
+            if (this.isMouseOverSlot(slot, mouseX, mouseY) && slot.func_111238_b()) {
+                this.theSlot = slot;
+                GL11.glDisable(GL11.GL_LIGHTING);
+                GL11.glDisable(GL11.GL_DEPTH_TEST);
+                int j1 = slot.xDisplayPosition;
+                k1 = slot.yDisplayPosition;
+                GL11.glColorMask(true, true, true, false);
+                this.drawGradientRect(j1, k1, j1 + 16, k1 + 16, -2130706433, -2130706433);
+                GL11.glColorMask(true, true, true, true);
+                GL11.glEnable(GL11.GL_LIGHTING);
+                GL11.glEnable(GL11.GL_DEPTH_TEST);
+            }
+        }
+
+        // Forge: Force lighting to be disabled as there are some issue where lighting would
+        // incorrectly be applied based on items that are in the inventory.
+        GL11.glDisable(GL11.GL_LIGHTING);
+        this.drawGuiContainerForegroundLayer(mouseX, mouseY);
+        GL11.glEnable(GL11.GL_LIGHTING);
+        InventoryPlayer inventoryplayer = this.mc.thePlayer.inventory;
+        ItemStack itemstack = this.draggedStack == null ? inventoryplayer.getItemStack() : this.draggedStack;
+
+        if (itemstack != null) {
+            byte b0 = 8;
+            k1 = this.draggedStack == null ? 8 : 16;
+            String s = null;
+
+            if (this.draggedStack != null && this.isRightMouseClick) {
+                itemstack = itemstack.copy();
+                itemstack.stackSize = MathHelper.ceiling_float_int((float) itemstack.stackSize / 2.0F);
+            } else if (this.field_147007_t && this.field_147008_s.size() > 1) {
+                itemstack = itemstack.copy();
+                itemstack.stackSize = this.field_146996_I;
+
+                if (itemstack.stackSize == 0) {
+                    s = "" + EnumChatFormatting.YELLOW + "0";
+                }
+            }
+
+            this.drawItemStack(itemstack, mouseX - k - b0, mouseY - l - k1, s);
+        }
+
+        if (this.returningStack != null) {
+            float f1 = (float) (Minecraft.getSystemTime() - this.returningStackTime) / 100.0F;
+
+            if (f1 >= 1.0F) {
+                f1 = 1.0F;
+                this.returningStack = null;
+            }
+
+            k1 = this.returningStackDestSlot.xDisplayPosition - this.field_147011_y;
+            int j2 = this.returningStackDestSlot.yDisplayPosition - this.field_147010_z;
+            int l1 = this.field_147011_y + (int) ((float) k1 * f1);
+            int i2 = this.field_147010_z + (int) ((float) j2 * f1);
+            this.drawItemStack(this.returningStack, l1, i2, (String) null);
+        }
+
+        GL11.glPopMatrix();
+
+        if (inventoryplayer.getItemStack() == null && this.theSlot != null && this.theSlot.getHasStack()) {
+            ItemStack itemstack1 = this.theSlot.getStack();
+            this.renderToolTip(itemstack1, mouseX, mouseY);
+        }
+
+        GL11.glEnable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        RenderHelper.enableStandardItemLighting();
     }
 
     @Override

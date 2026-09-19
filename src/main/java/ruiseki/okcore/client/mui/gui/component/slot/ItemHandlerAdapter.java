284069791f53ce17689639a1ca86c709b7223eb2
zyf051520 <zyf051520@foxmail.com>
2026-09-19 16:59:49 +0800

    fix: make ItemHandlerAdapter modifiable

--- src/main/java/ruiseki/okcore/client/mui/gui/component/slot/ItemHandlerAdapter.java
+++ src/main/java/ruiseki/okcore/client/mui/gui/component/slot/ItemHandlerAdapter.java
@@ -1,13 +1,15 @@
 package ruiseki.okcore.client.mui.gui.component.slot;
 
+import java.util.List;
+
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 
 import org.jetbrains.annotations.Nullable;
 
-import com.cleanroommc.modularui.utils.item.IItemHandler;
+import com.cleanroommc.modularui.utils.item.IItemHandlerModifiable;
 
-public class ItemHandlerAdapter implements IItemHandler {
+public class ItemHandlerAdapter implements IItemHandlerModifiable, ruiseki.okcore.item.handler.IItemHandlerModifiable {
 
     private final ruiseki.okcore.item.handler.IItemHandler internalHandler;
 
@@ -49,7 +51,21 @@ public class ItemHandlerAdapter implements IItemHandler {
     }
 
     @Override
+    public List<ItemStack> getStacks() {
+        return internalHandler.getStacks();
+    }
+
+    @Override
     public boolean isSlotFromInventory(int index, IInventory inventory, int invIndex) {
         return internalHandler.isSlotFromInventory(index, inventory, invIndex);
+    }
+
+    @Override
+    public void setStackInSlot(int slot, @Nullable ItemStack stack) {
+        if (internalHandler instanceof ruiseki.okcore.item.handler.IItemHandlerModifiable modifiable) {
+            modifiable.setStackInSlot(slot, stack);
+        } else {
+            throw new RuntimeException("Not modifiable!");
+        }
     }
 }

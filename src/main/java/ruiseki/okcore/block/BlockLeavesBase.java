d1731737e8cbb7bd799de2a358a45b991e71727b
GitHub GTNH Actions <>
2026-09-26 05:02:36 +0000

    spotlessApply

--- src/main/java/ruiseki/okcore/block/BlockLeavesBase.java
+++ src/main/java/ruiseki/okcore/block/BlockLeavesBase.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockLeaves;
 import net.minecraft.client.renderer.texture.IIconRegister;
@@ -19,6 +17,8 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import lombok.experimental.Delegate;
 import ruiseki.okcore.block.property.BlockProperty;
 import ruiseki.okcore.block.property.BlockPropertyProviderComponent;
